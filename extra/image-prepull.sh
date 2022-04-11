7b25430643cf07e7241e95207fcb5acba430b005
Brent Laster <bcl@nclasters.org>
2022-04-10 20:41:10 -0400

    Update pre-pull script

--- extra/image-prepull.sh
+++ extra/image-prepull.sh
@@ -1,3 +1,4 @@
 docker pull quay.io/techupskills/roar-web:1.0.1
 docker pull quay.io/techupskills/roar-db:1.0.1
 docker pull quay.io/techupskills/roar-debug:1.0.2
+docker pull quay.io/techupskills/roar-web:1.0.2
